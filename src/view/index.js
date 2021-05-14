b382f077d30a6ea9dc68c37499db0837a88701b3
Briceyda <Brd0208@gmail.com>
2021-05-14 10:40:05 -0500

    Delete header component seccion

--- src/view/index.js
+++ src/view/index.js
@@ -1,12 +1,9 @@
 import loginComponent from './loginComponent.js';
 import timelineComponent from './timelineComponent.js';
-// import headerComponent from './headerComponent.js';
 
 const components = {
   login: loginComponent,
   timeline: timelineComponent,
-  // header: headerComponent,
-
 };
 
 export {
